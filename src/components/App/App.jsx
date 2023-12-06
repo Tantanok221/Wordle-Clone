e41745ed139175aded61e56333b2a283658dfde8
Tan Kai <6572withspicy@gmail.com>
2023-12-06 23:39:44 +0800

    fix: fixed bug regarding vercel analytics not working

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
 import Game from '../Game';
 import Header from '../Header';
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   return (
     <div className="wrapper">
       <Header />
-
+      <Analytics />
       <div className="game-wrapper">
         <Game />
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react';
+
 
 
 
@@ -12,6 +12,6 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
+    
   </React.StrictMode>,
 )
